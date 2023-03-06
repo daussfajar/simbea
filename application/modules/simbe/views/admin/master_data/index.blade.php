dbfa835149e62dbd53a5bcf809c621d48928daf4
RakhmanY <yudazrakhman30@gmail.com>
2023-03-06 16:44:29 +0700

    tambah modal

--- application/modules/simbe/views/admin/master_data/index.blade.php
+++ application/modules/simbe/views/admin/master_data/index.blade.php
@@ -33,7 +33,7 @@
                 <tr>
                     <th>No</th>
                     <th>NIM</th>
-                    <th style="width: 20%">Nama Lengkap</th>
+                    <th style="width: 25%">Nama Lengkap</th>
                     <th>Program</th>
                     <th>Prodi</th>
                     <th>Status</th>
@@ -54,7 +54,8 @@
                     <td>{{ $data->nama_prodi }}</td>
                     <td>{{ $data->status }}</td>
                     <td class="text-center" style="vertical-align: middle;">
-                        <a href="#" class="m-1 btn btn-primary btn-sm"><i class="mdi mdi-pencil"></i></a>
+                        <button class="btn btn-primary btn-sm waves-effect waves-light" data-toggle="modal" data-target="#con-close-modal"><i class="mdi mdi-pencil"></i></button>
+                        {{-- <a href="#" class="m-1 btn btn-primary btn-sm" data-toggle="modal" data-target="#edit-modal"><i class="mdi mdi-pencil"></i></a> --}}
                         <a href="#" class="m-1 btn btn-danger btn-sm"><i class="mdi mdi-trash-can"></i></a>
                     </td>
                 </tr>
@@ -63,6 +64,76 @@
         </table>
     </div>
 
+    <!-- START MODAL EDIT -->
+    <div id="con-close-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title mt-0">Ubah Data</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="nim" class="control-label">NIM</label>
+                                <input type="text" class="form-control" id="nim" placeholder="Contoh:2023001001">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="field-2" class="control-label">Nama Lengkap</label>
+                                <input type="text" class="form-control" id="field-2" placeholder="Doe">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="field-3" class="control-label">Address</label>
+                                <input type="text" class="form-control" id="field-3" placeholder="Address">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="field-4" class="control-label">City</label>
+                                <input type="text" class="form-control" id="field-4" placeholder="Boston">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="field-5" class="control-label">Country</label>
+                                <input type="text" class="form-control" id="field-5" placeholder="United States">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="field-6" class="control-label">Zip</label>
+                                <input type="text" class="form-control" id="field-6" placeholder="123456">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group no-margin">
+                                <label for="field-7" class="control-label">Personal Info</label>
+                                <textarea class="form-control autogrow" id="field-7" placeholder="Write something about yourself" style="overflow: hidden; word-wrap: break-word; resize: horizontal; height: 104px;"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary waves-effect" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-info waves-effect waves-light">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END MODAL EDIT -->
     
     
 @endsection
--- application/modules/simbe/views/cache/53898487f3caa1b833cd8fc11272cba552bb71af.php
+++ application/modules/simbe/views/cache/53898487f3caa1b833cd8fc11272cba552bb71af.php
@@ -31,7 +31,7 @@
                 <tr>
                     <th>No</th>
                     <th>NIM</th>
-                    <th style="width: 20%">Nama Lengkap</th>
+                    <th style="width: 25%">Nama Lengkap</th>
                     <th>Program</th>
                     <th>Prodi</th>
                     <th>Status</th>
@@ -52,7 +52,8 @@
                     <td><?php echo e($data->nama_prodi); ?></td>
                     <td><?php echo e($data->status); ?></td>
                     <td class="text-center" style="vertical-align: middle;">
-                        <a href="#" class="m-1 btn btn-primary btn-sm"><i class="mdi mdi-pencil"></i></a>
+                        <button class="btn btn-primary btn-sm waves-effect waves-light" data-toggle="modal" data-target="#con-close-modal"><i class="mdi mdi-pencil"></i></button>
+                        
                         <a href="#" class="m-1 btn btn-danger btn-sm"><i class="mdi mdi-trash-can"></i></a>
                     </td>
                 </tr>
@@ -61,6 +62,76 @@
         </table>
     </div>
 
+    <!-- START MODAL EDIT -->
+    <div id="con-close-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title mt-0">Modal Content is Responsive</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="field-1" class="control-label">Name</label>
+                                <input type="text" class="form-control" id="field-1" placeholder="John">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="field-2" class="control-label">Surname</label>
+                                <input type="text" class="form-control" id="field-2" placeholder="Doe">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="field-3" class="control-label">Address</label>
+                                <input type="text" class="form-control" id="field-3" placeholder="Address">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="field-4" class="control-label">City</label>
+                                <input type="text" class="form-control" id="field-4" placeholder="Boston">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="field-5" class="control-label">Country</label>
+                                <input type="text" class="form-control" id="field-5" placeholder="United States">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="field-6" class="control-label">Zip</label>
+                                <input type="text" class="form-control" id="field-6" placeholder="123456">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group no-margin">
+                                <label for="field-7" class="control-label">Personal Info</label>
+                                <textarea class="form-control autogrow" id="field-7" placeholder="Write something about yourself" style="overflow: hidden; word-wrap: break-word; resize: horizontal; height: 104px;"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary waves-effect" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-info waves-effect waves-light">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END MODAL EDIT -->
     
     
 <?php $__env->stopSection(); ?>
